87d03bde2d18640ffb66040f216d9cd22a8f9f78
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 01:22:34 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO JOB (TITLE, COMPANY, LOCATION) VALUES ('Software Engineer', 'Accenture', 'Pune');
-INSERT INTO JOB (TITLE, COMPANY, LOCATION) VALUES ('Java Developer', 'TCS', 'Mumbai');
-INSERT INTO JOB (TITLE, COMPANY, LOCATION) VALUES ('Backend Developer', 'Infosys', 'Bangalore');
+INSERT INTO JOB (TITLE, COMPANY, LOCATION)
+VALUES ('Software Engineer', 'Accenture', 'Pune');
+
+INSERT INTO JOB (TITLE, COMPANY, LOCATION)
+VALUES ('Java Developer', 'TCS', 'Mumbai');
+
+INSERT INTO JOB (TITLE, COMPANY, LOCATION)
+VALUES ('Backend Developer', 'Infosys', 'Bangalore');
