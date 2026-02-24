161fb745118604f45fbd02c5641942488b26ee2b
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 00:49:36 +0530

    Create Job.java

---
+++ src/main/java/com/renuka/smartjobportal/entity/Job.java
@@ -0,0 +1,34 @@
+package com.renuka.smartjobportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String company;
+    private String location;
+
+    public Job() {}
+
+    public Job(String title, String company, String location) {
+        this.title = title;
+        this.company = company;
+        this.location = location;
+    }
+
+    public Long getId() { return id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getCompany() { return company; }
+    public void setCompany(String company) { this.company = company; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+}
