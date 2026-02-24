e454e0f01a793b23350deb28ed92854ef72f3f01
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 00:55:54 +0530

    Delete JobRepository.java

--- JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.renuka.smartjobportal.repository;
-
-import com.renuka.smartjobportal.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
