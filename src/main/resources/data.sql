9e94053c1630298e13013980a75218178a2ac43e
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 01:06:36 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO job (title, company, location) VALUES ('Software Engineer', 'Accenture', 'Pune');
+INSERT INTO job (title, company, location) VALUES ('Java Developer', 'TCS', 'Mumbai');
+INSERT INTO job (title, company, location) VALUES ('Backend Developer', 'Infosys', 'Bangalore');
