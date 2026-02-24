dbe831b2ac4153e58a673055dda70db1fc27fbd4
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 00:23:04 +0530

    Update SmartjobportalApplication.java

--- src/main/java/com/renuka/smartjobportal/SmartjobportalApplication.java
+++ src/main/java/com/renuka/smartjobportal/SmartjobportalApplication.java
@@ -1,1 +1,13 @@
 
+package com.renuka.smartjobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartjobportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartjobportalApplication.class, args);
+    }
+}
